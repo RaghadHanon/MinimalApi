95e1937ec99b1e94d1b21dbcafe9193e2bb4ada4
Raghad Hanon <raghadhanoon2015@gmail.com>
2024-12-10 19:01:00 +0200

    Add project

---
+++ MinimalApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Models;
+using MinimalApi.Services;
+
+namespace MinimalApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly JwtTokenGenerator _jwtTokenGenerator;
+    public AuthenticationController(JwtTokenGenerator jwtTokenGenerator)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator ??
+            throw new ArgumentNullException(nameof(jwtTokenGenerator));
+    }
+
+    [HttpPost("authenticate")]
+    public async Task<ActionResult<string>> Authenticate(
+        AuthenticationRequestBodyDto authenticationRequestBodyDto)
+    {
+        var token = await _jwtTokenGenerator.GenerateToken(authenticationRequestBodyDto);
+        if (token == null)
+        {
+            return Unauthorized();
+        }
+
+        return Ok(token);
+    }
+}
+
